a9ca601a3792d750bf34dfbd01cd707f0a56057c
Artpoke_ <arthuraugusto.arde@gmail.com>
2025-12-16 17:45:36 -0300

    📄 Recipe Tweak:  Silicon Unification

--- groovy/postInit/mods/ae2.groovy
+++ groovy/postInit/mods/ae2.groovy
@@ -13,3 +13,10 @@ ore_dict.remove('dustNetherQuartz', item('appliedenergistics2:material', 3))
 ore_dict.remove('dustQuartz', item('appliedenergistics2:material', 3))
 
 //=================================================================================================
+
+//Silicon Unification =============================================================================
+
+mods.jei.ingredient.hide(item('appliedenergistics2:material', 5))
+ore_dict.remove('itemSilicon', item('appliedenergistics2:material', 5))
+
+//=================================================================================================
