d361c8b328305eaec68de2ced919b3417ecf45d1
Artpoke_ <arthuraugusto.arde@gmail.com>
2025-12-16 15:25:06 -0300

    📦️ Mod Update:  Added Rockhound Chemistry.

--- groovy/postInit/mods/harvestcraft.groovy
+++ groovy/postInit/mods/harvestcraft.groovy
@@ -1,5 +1,14 @@
-//Unifying Salt ===================================================================================
+//Yeeting HarvestCraft's Machines =================================================================
 
-// crafting.removeByOutput(item('harvestcraft:saltitem'))
+def harvestcraft_machines = [
+    item('harvestcraft:waterfilter'),
+    item('harvestcraft:grinder'),
+    item('harvestcraft:presser')
+]
+
+for (machine in harvestcraft_machines) {
+    crafting.removeByOutput(machine)
+    mods.jei.ingredient.hide(machine)
+}
 
 //=================================================================================================
