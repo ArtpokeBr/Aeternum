86b87a619b6d83817532a7f76d5289236fe1d3ce
Artpoke_ <arthuraugusto.arde@gmail.com>
2025-12-17 15:07:35 -0300

    📄 Recipe Tweak:  Added Dried Kelp Smelting Compat with BOP Kelp.

---
+++ groovy/postInit/mods/bop.groovy
@@ -0,0 +1,14 @@
+//Kelp Tweaks =====================================================================================
+
+furnace.recipeBuilder()
+    .input(item('biomesoplenty:seaweed'))
+    .output(item('oe:dried_kelp'))
+    .exp(0.5)
+.register()
+
+mods.thermalexpansion.furnace.recipeBuilder()
+    .input(item('biomesoplenty:seaweed'))
+    .output(item('oe:dried_kelp'))
+.register()
+
+//=================================================================================================
