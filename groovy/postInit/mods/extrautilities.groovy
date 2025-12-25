3058966d26cf0e3335be3f3a44845184a5dc2f59
Artpoke_ <arthuraugusto.arde@gmail.com>
2025-12-24 22:17:01 -0300

    🛠️ Refactor:  Fully Yeeted XU2 Crusher through the usage of Mixins.

--- groovy/postInit/mods/extrautilities.groovy
+++ groovy/postInit/mods/extrautilities.groovy
@@ -1,9 +1,6 @@
 
-// Yeeting Furnace and Crusher ====================================================================
+// Yeeting Furnace ====================================================================
 
-mods.jei.ingredient.hide(item('extrautils2:machine').withNbt(['Type': 'extrautils2:crusher']))
-crafting.removeByOutput(item('extrautils2:machine').withNbt(['Type': 'extrautils2:crusher']))
-mods.extrautils2.crusher.removeAll()
 mods.jei.ingredient.hide(item('extrautils2:machine').withNbt(['Type': 'extrautils2:furnace']))
 crafting.removeByOutput(item('extrautils2:machine').withNbt(['Type': 'extrautils2:furnace']))
 mods.extrautils2.furnace.removeAll()
