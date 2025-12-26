202b3419824132183c8d44431c8c4c6200265066
Artpoke_ <arthuraugusto.arde@gmail.com>
2025-12-26 15:00:17 -0300

    📄 Recipe Tweak: Many Deeper Depths Recipe Additions and Amethyst Unification Fixes.

--- groovy/postInit/mods/deeperdepths.groovy
+++ groovy/postInit/mods/deeperdepths.groovy
@@ -28,3 +28,82 @@ for (item in copper_gear) {
 }
 
 //=================================================================================================
+
+// Amethyst Adjustments ===========================================================================
+
+mods.thermalexpansion.factorizer.removeByOutput(item('deeperdepths:amethyst_block'))
+crafting.removeByOutput(item('deeperdepths:amethyst_block'))
+crafting.shapedBuilder()
+    .output(item('deeperdepths:amethyst_block'))
+    .shape('AA',
+           'AA')
+    .key([A: item('deeperdepths:material', 1)])
+.register()
+crafting.shapelessBuilder()
+    .input([item('deeperdepths:amethyst_block')])
+    .output(item('deeperdepths:material', 1) * 4)
+.register()
+
+ore_dict.add('gemAmethyst', item('deeperdepths:material', 1))
+
+//BOP Amethyst
+mods.thermalexpansion.factorizer.removeByOutput(item('biomesoplenty:gem_block'))
+crafting.removeByOutput(item('biomesoplenty:gem_block'))
+crafting.shapedBuilder()
+    .output(item('biomesoplenty:gem_block'))
+    .shape('AAA',
+           'AAA',
+           'AAA')
+    .key([A: item('biomesoplenty:gem')])
+.register()
+
+//Roots Amethyst
+crafting.shapedBuilder()
+    .output(item('mysticalworld:amethyst_block'))
+    .shape('AAA',
+           'AAA',
+           'AAA')
+    .key([A: item('mysticalworld:amethyst_gem')])
+.register()
+crafting.shapelessBuilder()
+    .input([item('mysticalworld:amethyst_block')])
+    .output(item('mysticalworld:amethyst_gem') * 9)
+.register()
+
+//=================================================================================================
+
+// Calcite Adjustments ============================================================================
+def calcite_dust = item('rockhounding_chemistry:chemical_dusts', 23)
+
+mods.thermalexpansion.pulverizer.recipeBuilder()
+    .input(ore('stoneCalcite'))
+    .output(calcite_dust, calcite_dust)
+    .chance(10)
+.register()
+
+mods.enderio.sag_mill.recipeBuilder()
+    .input(ore('stoneCalcite'))
+    .output(calcite_dust)
+    .output(calcite_dust, 0.1)
+    .tierAny()
+.register()
+
+mods.appliedenergistics2.grinder.recipeBuilder()
+    .input(ore('stoneCalcite'))
+    .output(calcite_dust, calcite_dust)
+    .turns(5)
+    .chance1(0.1)
+.register()
+
+//=================================================================================================
+
+// Deepslate to Slate =============================================================================
+
+mods.botania.mana_infusion.recipeBuilder()
+    .input(ore('stoneDeepslate'))
+    .output(item('rustic:slate'))
+    .mana(100)
+    .catalyst(blockstate('botania:alchemycatalyst'))
+.register()
+
+//=================================================================================================
