68b9f6c801952a1e861b60f05c40bb2f43d49f00
Artpoke_ <arthuraugusto.arde@gmail.com>
2026-05-12 19:10:33 -0300

    📉 Mod Removal:  Underground Biomes

--- groovy/postInit/mods/undergroundbiomes.groovy
+++ groovy/postInit/mods/undergroundbiomes.groovy
@@ -1,9 +1,9 @@
 
 //Adding Chisel Compat to Underground's Stones
 //mods.chisel.carving.addVariation("marble", item('undergroundbiomes:metamorphic_stone', 2))
-mods.chisel.carving.addVariation("limestone", item('undergroundbiomes:sedimentary_stone'))
-mods.chisel.carving.addVariation("basalt", item('undergroundbiomes:igneous_stone', 5))
+// mods.chisel.carving.addVariation("limestone", item('undergroundbiomes:sedimentary_stone'))
+// mods.chisel.carving.addVariation("basalt", item('undergroundbiomes:igneous_stone', 5))
 
 //Converting Marble into Usable Marble.
-crafting.shapelessBuilder().output(item('undergroundbiomes:metamorphic_stone', 2)).input([ore('blockMarble')]).register()
-crafting.shapelessBuilder().output(item('astralsorcery:blockmarble')).input([item('undergroundbiomes:metamorphic_stone', 2)]).register()
+// crafting.shapelessBuilder().output(item('undergroundbiomes:metamorphic_stone', 2)).input([ore('blockMarble')]).register()
+// crafting.shapelessBuilder().output(item('astralsorcery:blockmarble')).input([item('undergroundbiomes:metamorphic_stone', 2)]).register()
